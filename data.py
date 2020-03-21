c698894c5eff848dc443b5198ec9581210055ff1
OsPy303 <60208316+OsPy303@users.noreply.github.com>
2020-03-21 20:52:43 +0800

    Add files via upload

---
+++ data.py
@@ -0,0 +1,61 @@
+
+
+DEFAULT_SCREEN_SIZE = (500, 700)
+
+# List of all of questions
+QUESTION = 'question.txt'
+
+# Sound on false answere
+FALSE_ANSWERE = "sound/error.wav"
+
+# Sound on true answere
+TRUE_ANSWERE = "sound/true.wav"
+
+# Background sound
+SOUND_BACKGROUND = "sound/music.wav"
+
+# Displayed image when Background sound off
+IMG_SOUND_OFF = 'image/sound_off.png'
+
+# Displayed image when Background sound on
+IMG_SOUND_ON = 'image/sound_on.png'
+
+# Displayed image when Background sound option pressed
+IMG_SOUND_PRESSED = 'image/sound_down.png'
+
+# Displayed home home backgroud image
+HOME_BACKGROUND = "image/back88.jpg"
+
+# Displayed game, about, score, game over backgroud
+#BACKGROUND = "image/back_game2.png"
+BACKGROUND = "image/back_game222.png"
+
+# Life image
+LIFE = 'image/life1.png'
+
+# Home title
+HOME_TITLE = "image/titre1.png"
+
+# Play button
+PLAY_UP = 'image/play_normal.png'
+PLAY_DOWN = 'image/play_down.png'
+
+# Score button
+SCORE_UP = 'image/score_normal.png'
+SCORE_DOWN = 'image/about_down.png'
+
+# About button
+ABOUT_UP = 'image/about_normal.png'
+ABOUT_DOWN = 'image/about_down.png'
+
+# Exit button
+EXIT_UP = 'image/quit_normal.png'
+EXIT_DOWN = 'image/quit_down.png'
+
+# Home Button
+HOME_UP = 'image/home_normal.png'
+HOME_DOWN = 'image/home_down.png'
+
+# Reset score button
+RESET_UP = 'image/reset_normal.png'
+RESET_DOWN = 'image/reset_down.png'
